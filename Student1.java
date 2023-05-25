92a6730be9fe0abd0592e7c4fcae6f685d56cf63
Vikas Patkar <vikkipatkar08@gmail.com>
2023-05-26 01:00:21 +0530

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,15 @@
+package com.constructor;
+
+public class Student1 {
+    //trying  explicit initialization
+
+    private int roll = 10;  //          This is how
+    private String name = "vikas";//    Explicit initialization
+    private double per = 67.8;//        looks like
+
+
+       public void show()
+       {
+           System.out.println("roll = "+roll+", Name = "+name+",Percentage = "+per);
+       }
+}
---
+++ UseStudent1.java
@@ -0,0 +1,8 @@
+package com.constructor;
+
+public class UseStudent1 {
+    public static void main(String[] args) {
+        Student1 s= new Student1();
+        s.show();
+    }
+}
